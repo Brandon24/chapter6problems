26d7fcceb3faaa4a3bf9fb9c20a3aa04100893dc
Brandon24 <brandonreinert24@gmail.com>
2014-05-06 10:07:01 -0400

    Garyson likes poopy

---
+++ chapter6problems/Numbers.cs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Numbers.cs
+{
+    class Program
+    {
+        public static void sum(int a, int b)
+        {
+            Console.WriteLine(a+b);
+        }
+        public static void difference(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+        static void Main(string[] args)
+        {
+            int q = 30, t = 39;
+            sum(q, t);
+            difference(q, t);
+
+            
+
+        }
+    }
+}
---
+++ chapter6problems/Numbers2.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Numbers2.cs
+{
+    class Program
+    {
+        public static int product(int a, int b)
+        {
+            return a * b;
+        }
+        static void Main(string[] args)
+        {
+            int q = 30, t = 39;
+            int result = product (q, t);
+            Console.WriteLine(result);
+            
+            
+
+            
+        }
+    }
+}
